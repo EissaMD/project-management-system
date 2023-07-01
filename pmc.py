ad65183314e819923813ba8efff3a16eab96f038
EissaMD <eissa.code@gmail.com>
2023-07-01 13:17:16 +0300

    Change secondary menu pos to be below the top menu insead of left

--- pmc.py
+++ pmc.py
@@ -8,18 +8,27 @@ class App(ttk.Window):
             size=(700, 600),
             resizable=(True, False),
         )
-        # configure grid layout (2x1)
-        # ============ create top frames ============
-        top_frame = ttk.Frame(master=self,height=50 , bootstyle="secondary")
+        # ============ create top menu ============
+        top_frame = ttk.Frame(master=self,height=400 , bootstyle="secondary")
         top_frame.pack(side="top" , fill="x" ,anchor="center")
-        ttk.Label(top_frame,text= "Project Management System" ,borderwidth=2, bootstyle="inverse-secondary").pack(side="top")
-        left_frame = ttk.Frame(master=self,width=150 , bootstyle="info")
-        left_frame.pack(side="left" , fill="y" ,anchor="w")
-        self.right_frame = ttk.Frame(master=self, bootstyle="light")
-        self.right_frame.pack( fill="both" ,expand=True ,side="right")
-        
-        
+        main_menu = {"Project":  1,
+                     "Supplier":   1,
+                     "Customer":  1,
+                     "Reports":  1,
+                     }
+        for btn_name , btn_func in main_menu.items():
+            ttk.Button(top_frame,text=btn_name).pack(side="left")
+        self.secondary_menu = ttk.Frame(master=self, bootstyle="info")
+        self.secondary_menu.pack(side="top" , fill="x" ,anchor="w")
+        frame=ttk.Frame(self.secondary_menu, bootstyle="info"); frame.pack()
+        ttk.Button(frame,text="btn_name" , bootstyle="info").pack(side="left" , padx=1,anchor="e")
+        ttk.Button(frame,text="btn_name" , bootstyle="info").pack(side="left" , padx=1,anchor="e")
+        # ============ Create body frame ============
+        self.body_frame = ttk.Frame(master=self, bootstyle="light")
+        self.body_frame.pack( fill="both" ,expand=True)
 
+        
+        
 if __name__ == "__main__":
     app = App()
     app.mainloop()
