7fa608b2c977affdeb4abd79d50170f2848234d3
EissaMD <eissa.code@gmail.com>
2023-07-01 17:14:56 +0300

    Create NewPage class to generate new page every time 2nd menu clicked

--- pmc.py
+++ pmc.py
@@ -9,26 +9,52 @@ class App(ttk.Window):
             resizable=(True, False),
         )
         # ============ create top menu ============
-        top_frame = ttk.Frame(master=self,height=400 , bootstyle="secondary")
-        top_frame.pack(side="top" , fill="x" ,anchor="center")
+        top_frame = ttk.Frame(master=self,height=400, bootstyle="secondary")
+        top_frame.pack(side="top" , fill="x" ,anchor="center" )
         main_menu = {"Project":  1,
                      "Supplier":   1,
                      "Customer":  1,
                      "Reports":  1,
                      }
         for btn_name , btn_func in main_menu.items():
-            ttk.Button(top_frame,text=btn_name).pack(side="left")
+            ttk.Button(top_frame,text=btn_name ).pack(side="left")
         self.secondary_menu = ttk.Frame(master=self, bootstyle="info")
         self.secondary_menu.pack(side="top" , fill="x" ,anchor="w")
         frame=ttk.Frame(self.secondary_menu, bootstyle="info"); frame.pack()
-        ttk.Button(frame,text="btn_name" , bootstyle="info").pack(side="left" , padx=1,anchor="e")
-        ttk.Button(frame,text="btn_name" , bootstyle="info").pack(side="left" , padx=1,anchor="e")
         # ============ Create body frame ============
         self.body_frame = ttk.Frame(master=self, bootstyle="light")
         self.body_frame.pack( fill="both" ,expand=True)
+        page = NewPage(self,self.secondary_menu,self.body_frame)
+        menu = {"New Project":      1,
+                "Edit Project":     1,
+                "Delete Project":   1,
+                }
+        page.create_secondary_menu(menu)
+        page.create_body("Add New Project")
+##############################################################################################################
 
-        
-        
+class NewPage():
+    def __init__(self,main_window,secondary_menu,body_frame):
+        self.main_window = main_window
+        self.secondary_menu = secondary_menu
+        self.body_frame = body_frame
+    ###############        ###############        ###############        ###############
+    def create_body(self , title="No Title"):
+        self.body_frame.destroy()
+        self.body_frame = ttk.Frame(master=self.main_window)
+        self.body_frame.pack( fill="both" ,expand=True)
+        ttk.Label(self.body_frame,font="Times 25 bold" ,text=title ).pack(pady=6)
+
+    ###############        ###############        ###############        ###############
+    def create_secondary_menu(self , menu):
+        self.secondary_menu.destroy()
+        self.secondary_menu = ttk.Frame(master=self.main_window, bootstyle="info")
+        self.secondary_menu.pack(side="top" , fill="x" ,anchor="w")
+        frame=ttk.Frame(self.secondary_menu, bootstyle="info"); frame.pack()
+        for btn_name , btn_func in menu.items():
+            ttk.Button(frame,text=btn_name , bootstyle="info").pack(side="left")
+##############################################################################################################
+
 if __name__ == "__main__":
     app = App()
     app.mainloop()
